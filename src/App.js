86c24da2bc0b84f54c2e64ce6e6035a69bd8a914
AmiAgiGit <65073014+AmiAgiGit@users.noreply.github.com>
2020-10-04 10:46:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hello there how are you doing today 
           </a>
         </header>
       </div>
